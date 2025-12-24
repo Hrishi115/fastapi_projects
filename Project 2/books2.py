b5fdf5535e8e8f291a804b195c251d941713b9a5
Hrishikesh <hrishikeshshendage313@gmail.com>
2025-12-25 00:25:47 +0530

    add pydantic working and few basic get and put methods

---
+++ Project 2/books2.py
@@ -0,0 +1,69 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating  = rating
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description = "Id is not always needed", default = None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=2)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1,lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "Book title",
+                "author": "Book author",
+                "description": "A book description",
+                "rating": 5
+            }
+        }
+    }
+
+BOOKS = [
+    Book(1, "Computer Science", "CC Coder", "Nice Book!", 5),
+    Book(2, "Computer Science 1", "AB Coder", "Good Book!", 5),
+    Book(3, "Computer Science 2", "CE Coder", "Better Book!", 5),
+    Book(4, "Computer Science 3", "CD Coder", "Not so bad Book!", 5),
+    Book(5, "Computer Science 4", "C Coder", "Very Nice Book!", 5),
+]
+
+def set_book_id(book : Book):
+    if len(BOOKS) > 0:
+        book.id = BOOKS[-1].id + 1
+    else:
+        book.id = 1
+    return book
+
+@app.get("/books/{book_id}")
+async def get_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+@app.post("/books/create_book")
+async def create_book(book_request:BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(set_book_id(new_book))
+
