aea7e0c8f97c6d72cb237cce459316dcf8d93e6f
Hrishikesh <hrishikeshshendage313@gmail.com>
2025-12-26 15:08:11 +0530

    add an endpoint to retrieve books by publishing date

--- Project 2/books2.py
+++ Project 2/books2.py
@@ -8,13 +8,15 @@ app = FastAPI()
 class Book:
     id: int
     title: str
+    published_date: int
     author: str
     description: str
     rating: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, published_date,author, description, rating):
         self.id = id
         self.title = title
+        self.published_date = published_date
         self.author = author
         self.description = description
         self.rating  = rating
@@ -22,6 +24,7 @@ class Book:
 class BookRequest(BaseModel):
     id: Optional[int] = Field(description = "Id is not always needed", default = None)
     title: str = Field(min_length=3)
+    published_date:int = Field(min_length=2)
     author: str = Field(min_length=2)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1,lt=6)
@@ -30,6 +33,7 @@ class BookRequest(BaseModel):
         "json_schema_extra": {
             "example": {
                 "title": "Book title",
+                "published_date": "Published date",
                 "author": "Book author",
                 "description": "A book description",
                 "rating": 5
@@ -38,11 +42,11 @@ class BookRequest(BaseModel):
     }
 
 BOOKS = [
-    Book(1, "Computer Science", "CC Coder", "Nice Book!", 5),
-    Book(2, "Computer Science 1", "AB Coder", "Good Book!", 5),
-    Book(3, "Computer Science 2", "CE Coder", "Better Book!", 3),
-    Book(4, "Computer Science 3", "CD Coder", "Not so bad Book!", 5),
-    Book(5, "Computer Science 4", "C Coder", "Very Nice Book!", 5),
+    Book(1, "Computer Science", 2012,"CC Coder", "Nice Book!", 5),
+    Book(2, "Computer Science 1", 2013,"AB Coder", "Good Book!", 5),
+    Book(3, "Computer Science 2", 2022,"CE Coder", "Better Book!", 3),
+    Book(4, "Computer Science 3", 2023,"CD Coder", "Not so bad Book!", 5),
+    Book(5, "Computer Science 4", 2025,"C Coder", "Very Nice Book!", 5),
 ]
 
 def set_book_id(book : Book):
@@ -71,8 +75,22 @@ async def get_book(book_id: int):
             return book
 
 
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
+
+@app.get("/books/published/{published_date}")
+async def get_by_published_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
+
 @app.post("/books/create_book")
 async def create_book(book_request:BookRequest):
     new_book = Book(**book_request.model_dump())
     BOOKS.append(set_book_id(new_book))
-
