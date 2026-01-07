846d3922ecce74f7324c8db2a4bf58d3dd781968
Hrishikesh <hrishikeshshendage313@gmail.com>
2026-01-07 22:00:42 +0530

    Add first API endpoint to read all data from the DB

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from models import Todos
 import models
-from database import engine
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependancy = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependancy):
+    return db.query(Todos).all()
